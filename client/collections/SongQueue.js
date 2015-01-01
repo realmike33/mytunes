e41bf9a73e6727869a5e9d7567302ae79171457a
realmike33 <realmike33@gmail.com>
2014-12-31 17:38:01 -0800

    almost completed

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,21 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
+    this.counter = 0;
+
+  },
+
+  playFirst: function() {
+    if (this.counter < 1) {
+      console.log(this);
+      this.at(0).play();
+    }
+
   }
+  // change currentSong to nextSong when currenSong ends.  This should fire an event and autoplay next one
+
+  // enqueue: function() {
+
+  // }
 
 });
